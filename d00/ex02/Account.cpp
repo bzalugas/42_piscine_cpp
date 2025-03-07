e4f025c30e4181e92ecfc68aa387a9bd1cd4e310
bazaluga <bazaluga@student.42.fr>
2025-03-07 08:44:17 +0100

    Begin ex02

--- d00/ex02/Account.cpp
+++ d00/ex02/Account.cpp
@@ -6,11 +6,30 @@
 //   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/02/20 17:45:15 by bazaluga          #+#    #+#             //
-//   Updated: 2025/02/20 17:50:27 by bazaluga         ###   ########.fr       //
+//   Updated: 2025/03/05 11:13:24 by bazaluga         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+    this->_accountIndex = Account::_nbAccounts++;
+    this->_amount = initial_deposit;
+    this->_nbDeposits = 1;
+}
+
+Account::~Account(void)
+{
+    Account::_nbAccounts--;
+};
 
 int     Account::getNbAccounts(void)
 {
@@ -32,7 +51,24 @@ int     Account::getNbWithdrawals(void)
     return (_totalNbWithdrawals);
 }
 
-void    Account::displayAccountsInfos(void)
+void	Account::_displayTimestamp(void)
 {
+    std::time_t	timestamp;
+    struct tm		date;
+    char			s[18];
 
+    std::time(&timestamp);
+    date = *localtime(&timestamp);
+	std::strftime(s, 18, "[%Y%m%d_%H%M%S]", &date);
+    std::cout << s << std::endl;
 }
+
+void    Account::displayStatus(void) const
+{
+    this->_displayTimestamp();
+}
+
+// void    Account::displayAccountsInfos(void)
+// {
+
+// }
---
+++ d00/ex02/test.cpp
@@ -0,0 +1,9 @@
+#include "Account.hpp"
+
+int main()
+{
+    Account a(23);
+
+    a.displayStatus();
+    return 0;
+}
